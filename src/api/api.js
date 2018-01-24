d82a1211a771100634659f529933c886309a8bdb
lollipop7 <juanlilu@yeah.net>
2018-01-24 09:07:09 +0800

    backup

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import {
+    wxRequest
+} from '../utils/wxrequest'; 
+
+const apimall = 'http://192.168.1.251:8089/';
+
+//首页获取猎头职位
+const getHunterjob = (params) => wxRequest(params, apimall + 'mobile/api/hunterjob');
+
+module.exports = {
+    getHunterjob
+}
+  
