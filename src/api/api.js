aedff974dc23e00e6af36a3c194aff0cad38c8e9
yangdoubleming <yangdoubleming@163.com>
2018-01-26 17:51:56 +0800

    公司-公司详情改为接口拉取数据

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import {
 } from '../utils/wxrequest'; 
 
 const apimall = 'http://192.168.1.251:8089/';
+// const apimall = 'https://m.51jrq.com/';
 
 //首页获取猎头职位
  const  getHunterjob = (params) => wxRequest(params, apimall + 'mobile/api/hunterjob');
@@ -10,9 +11,12 @@ const apimall = 'http://192.168.1.251:8089/';
  //查看职位详细
  const viewHunterjob = (params) => wxRequest(params, apimall + 'mobile/api/hunterjob');
 
+//  公司-获取公司列表及公司详情
+ const getCorpListView = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
 
 module.exports = {
     getHunterjob, 
-    viewHunterjob
+    viewHunterjob,
+    getCorpListView
 }
   
