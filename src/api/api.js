c8d3d31e1d2d0fdbbd4eb6af2e7daf58a1d209cf
lollipop7 <juanlilu@yeah.net>
2018-01-29 20:19:23 +0800

    搜索页

--- src/api/api.js
+++ src/api/api.js
@@ -5,18 +5,10 @@ import {
 const apimall = 'http://192.168.1.251:8089/';
 // const apimall = 'https://m.51jrq.com/';
 
-//首页获取猎头职位
+//获取猎头职位
  const  getCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
 
- //查看职位详细
- const viewCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
-
-//  公司-获取公司列表及公司详情
- const getCorpListView = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
-
 module.exports = {
-    getCompanyjob, 
-    viewCompanyjob,
-    getCorpListView
+    getCompanyjob 
 }
   
