cf8fa9ab69b7a3d06737b794642ef69e20defee0
yangdoubleming <yangdoubleming@163.com>
2018-01-30 17:05:48 +0800

    我的-收藏及投递状态

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,11 @@ const apimall = 'http://192.168.1.251:8089/';
 //获取猎头职位
  const  getCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
 
+//  获取我的收藏及投递记录
+const  getCollectJob = (params) => wxRequest(params, apimall + 'mobile/api/persoanl');
+
 module.exports = {
-    getCompanyjob 
+    getCompanyjob,
+    getCollectJob 
 }
   
---
+++ src/data/collect-data.js
@@ -0,0 +1,10 @@
+const collectData = {
+    "datanum": 2,
+    "returnCode": "AAAAAAA",
+    "returnMsg": "操作成功",
+    "data": "[{\"jobcollectionid\":\"23\",\"jobid\":\"42\",\"uid\":\"39\",\"jobname\":\"投融资总监\",\"corpname\":\"东吴期货\",\"jobcity\":\"上海\",\"companyid\":\"39\",\"jobtype\":0,\"issuedate\":1455700863000,\"status\":\"2\",\"salary\":\"10k-15k\",\"refreshdate\":\"2015-08-03 04:00:00.0\",\"createby\":\"1060024\",\"createdate\":\"2014-12-27\",\"activeflag\":1},{\"jobcollectionid\":\"23\",\"jobid\":\"42\",\"uid\":\"39\",\"jobname\":\"投融资总监\",\"corpname\":\"东吴期货\",\"jobcity\":\"上海\",\"companyid\":\"39\",\"jobtype\":0,\"issuedate\":1455700863000,\"status\":\"2\",\"salary\":\"10k-15k\",\"refreshdate\":\"2015-08-03 04:00:00.0\",\"createby\":\"1060024\",\"createdate\":\"2014-12-27\",\"activeflag\":1}]"
+}
+
+module.exports = {
+    collectData: collectData
+}
