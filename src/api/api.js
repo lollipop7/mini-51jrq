1be31fe52456f65e54d034ca4470b7ecfd47d41c
lollipop7 <juanlilu@yeah.net>
2018-01-31 18:18:54 +0800

    搜索页

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,16 @@ import {
 
 const apimall = 'http://192.168.1.251:8089/';
 // const apimall = 'https://m.51jrq.com/';
+const apiGps = 'http://apis.map.qq.com/ws/geocoder/v1'
 
 //获取猎头职位
- const  getCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
+const  getCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
+
+//逆地址解析
+const getCityName = (params) => wxRequest(params, apiGps);
 
 module.exports = {
-    getCompanyjob 
+    getCompanyjob,
+    getCityName
 }
   
