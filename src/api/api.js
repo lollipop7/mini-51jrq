a893be2380db8761f3a41a1b31edaece8d44dd5a
yangdoubleming <yangdoubleming@163.com>
2018-02-09 15:59:13 +0800

    我的简历部分修改

--- src/api/api.js
+++ src/api/api.js
@@ -2,36 +2,36 @@ import {
     wxRequest
 } from '../utils/wxrequest'; 
 
-// const apimall = 'http://192.168.1.251:8089/';
-const apimall = 'https://m.51jrq.com/';
+const apimall = 'http://192.168.1.251:8089/mobile';
+// const apimall = 'https://m.51jrq.com/mobile2';
 const apiGps = 'http://apis.map.qq.com/ws/geocoder/v1'
 
 //得到首页banner图片列表
-const getMobileHomeBanner = (params) => wxRequest(params, apimall + 'mobile/api/ad_show/getMobileHomeBanner');
+const getMobileHomeBanner = (params) => wxRequest(params, apimall + '/api/ad_show/getMobileHomeBanner');
 
 //获取猎头职位
-const  getCompanyjob = (params) => wxRequest(params, apimall + 'mobile/api/companyjob');
+const  getCompanyjob = (params) => wxRequest(params, apimall + '/api/companyjob');
 
 //逆地址解析
 const getCityName = (params) => wxRequest(params, apiGps);
 
 //  获取我的收藏及投递记录
-const  getCollectJob = (params) => wxRequest(params, apimall + 'mobile/api/persoanl');
+const  getCollectJob = (params) => wxRequest(params, apimall + '/api/persoanl');
 
 // 获取简历信息
-const  getResumeInfo = (params) => wxRequest(params, apimall + 'mobile/api/my');
+const  getResumeInfo = (params) => wxRequest(params, apimall + '/api/my');
 
 // 获取数据字典数据
-const  getDictData = (params) => wxRequest(params, apimall + 'mobile/api/dictionary');
+const  getDictData = (params) => wxRequest(params, apimall + '/api/dictionary');
 
 // 用户登录
-const  loginIn = (params) => wxRequest(params, apimall + 'mobile/api/login');
+const  loginIn = (params) => wxRequest(params, apimall + '/api/login');
 
 // 个人用户注册获取短信验证码
-const  getVerifyCode = (params) => wxRequest(params, apimall + 'mobile/api/regphone');
+const  getVerifyCode = (params) => wxRequest(params, apimall + '/api/regphone');
 
 // 修改头像
-const  changeHeadImg = (params) => wxRequest(params, apimall + 'mobile/api/pimg');
+const  changeHeadImg = (params) => wxRequest(params, apimall + '/api/pimg');
 
 module.exports = {
     getMobileHomeBanner,
