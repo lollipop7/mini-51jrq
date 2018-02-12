d88fbebb700e45e58a92c3cfd865a7404156d24d
yangdoubleming <yangdoubleming@163.com>
2018-02-12 13:25:42 +0800

    登录后跳转调整

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,9 @@
 export const goLogin = function(){
     wx.getStorage({
         key: 'loginData',
+        success: function(res){
+            console.log(res)
+        },
         fail: function(){
             wx.navigateTo({
                 url: '../login/login'
