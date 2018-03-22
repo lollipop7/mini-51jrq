6992615043da85f74f535babc3175abec2c8052b
yangdoubleming <yangdoubleming@163.com>
2018-03-22 18:01:48 +0800

    投递状态功能模块修改

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,9 @@ import {
     wxRequest
 } from '../utils/wxrequest'; 
 
-// const apimall = 'http://192.168.1.251:8089/mobile';
+const apimall = 'http://192.168.1.251:8089/mobile';
 // const apimall = 'http://192.168.1.226:8080/mobile';
-const apimall = 'https://m.51jrq.com/mobile2'; 
+// const apimall = 'https://m.51jrq.com/mobile2'; 
 const apiGps = 'https://apis.map.qq.com/ws/geocoder/v1';
 
 //得到首页banner图片列表
@@ -58,6 +58,9 @@ const  importResume = (params) => wxRequest(params, apimall + '/api/importResume
 // 找回密码
 const  resetPassword = (params) => wxRequest(params, apimall + '/api/password_old');
 
+// 简历投递流程
+const  getProcessView = (params) => wxRequest(params, apimall + '/api/ai/deliveryprocess');
+
 module.exports = {
     apimall,
     getMobileHomeBanner,
@@ -76,6 +79,7 @@ module.exports = {
     getVerifyCode,
     importResume,
     changeHeadImg,
-    resetPassword
+    resetPassword,
+    getProcessView
 }
   
