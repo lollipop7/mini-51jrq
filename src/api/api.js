1e745f1c72d78b0ffd5f4ac5cb7543dd627cfc0e
lollipop7 <juanlilu@yeah.net>
2018-03-28 11:45:31 +0800

    backup

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import {
     wxRequest
 } from '../utils/wxrequest'; 
 
-// const apimall = 'http://192.168.1.251:8089/mobile';
-// const apimall = 'http://192.168.1.226:8080/mobile';
+// const apimall = 'http://192.168.1.251:8089/mobile'; 
+// const apimall = 'http://192.168.1.226:8080/mobile'; 
 const apimall = 'https://m.51jrq.com/mobile2'; 
 const apiGps = 'https://apis.map.qq.com/ws/geocoder/v1';
 
